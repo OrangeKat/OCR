c53211b60617e57fe3b42658d00dae5a4f7cba4b
thomas.polo <thomas.polo@epita.fr>
2022-12-10 22:28:42 +0100

    Final commit

--- neural_network/neural_network.h
+++ neural_network/neural_network.h
@@ -11,11 +11,11 @@
     - number of training sets
 */
 
-#define num_inputs 256
-#define num_hidden 256
+#define num_inputs 576
+#define num_hidden 576
 #define num_output 10
 double learning_rate = 0.1f;
-#define res 16
+#define res 24
 #define num_training_sets 10
 
 /*
