5612f5235bd6c6822907629cf272bdba74abbf3c
thomas.polo <thomas.polo@epita.fr>
2022-12-10 18:55:43 +0100

    a

--- split_grid/split_grid.c
+++ split_grid/split_grid.c
@@ -2,32 +2,7 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 
-//function that splits a png of a sudoku grid into 81 images of the individual cells
-void split_image(char *filename){
-    SDL_Surface *image = IMG_Load(filename);
-    int height = image->h;
-    int width = image->w;
-    int cell_height = height/9;
-    int cell_width = width/9;
-    int x = 0,y = 0;
-    int n = 1;
-    for (int i = 0; i < 9; i++){
-        for (int j = 0; j < 9; j++){
-            SDL_Rect rect = {x,y,cell_width,cell_height};
-            SDL_Surface *cell = SDL_CreateRGBSurface(0,cell_width,cell_height,32,0,0,0,0);
-            SDL_BlitSurface(image,&rect,cell,NULL);
-            remove_border_lines(cell);
-            char *cell_name = malloc(28);
-	        sprintf(cell_name, "output/cell_%d.png",n);
-            IMG_SavePNG(cell,cell_name);
-            x += cell_width;
-            n++;
-        }
-        x = 0;
-        y += cell_height;
-    }
-}
-
+// Function that removes the border lines from a cell
 void remove_border_lines(SDL_Surface *surface) {
     SDL_LockSurface(surface);
 
@@ -85,6 +60,32 @@ void remove_border_lines(SDL_Surface *surface) {
 
     // Replace the original surface with the new one
     *surface = *new_surface;
+}
+
+//function that splits a png of a sudoku grid into 81 images of the individual cells
+void split_image(char *filename){
+    SDL_Surface *image = IMG_Load(filename);
+    int height = image->h;
+    int width = image->w;
+    int cell_height = height/9;
+    int cell_width = width/9;
+    int x = 0,y = 0;
+    int n = 1;
+    for (int i = 0; i < 9; i++){
+        for (int j = 0; j < 9; j++){
+            SDL_Rect rect = {x,y,cell_width,cell_height};
+            SDL_Surface *cell = SDL_CreateRGBSurface(0,cell_width,cell_height,32,0,0,0,0);
+            SDL_BlitSurface(image,&rect,cell,NULL);
+            remove_border_lines(cell);
+            char *cell_name = malloc(28);
+	        sprintf(cell_name, "output/cell_%d.png",n);
+            IMG_SavePNG(cell,cell_name);
+            x += cell_width;
+            n++;
+        }
+        x = 0;
+        y += cell_height;
+    }
 }
 
 int main(int argc, char *argv[]){
